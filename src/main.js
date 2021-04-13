1120800e8cf3bae893777299c3038d84d8b1ac5f
Nicholas Reeder <nickreeder32@gmail.com>
2021-04-13 06:41:33 -0700

    add jest and passing test for triangle object creation

--- src/main.js
+++ src/main.js
@@ -1,3 +1,10 @@
+import Triangle from './triangle.js';
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+import './styles.css';
+
 $(document).ready(function () {
   $('#triangle-checker-form').submit(function (event) {
     event.preventDefault();
